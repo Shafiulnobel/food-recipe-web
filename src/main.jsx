7e5be38f87161dcc7e3e38e0b4d0050329b5741f
Shafiulnobel <alfrednobel636@gmail.com>
2024-01-18 03:24:08 +0600

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-import Main from './Layout/Main.jsx'
 import router from './Routes/Routes.jsx'
 import { RouterProvider } from 'react-router-dom'
 
